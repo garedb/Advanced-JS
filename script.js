018e3885a49e42c286c30dca42315d3bb20343d3
garedb <garedbarney@gmail.com>
2020-09-24 14:15:06 -0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-//
+/*************************************
+// Function constructor/ Prototypes
+*/
+
+
+// let john = {
+//   name: 'John',
+//   yearOfBirth: 1990,
+//   job: 'teacher'
+// };
+
+let Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth)
+  }
+
+Person.prototype.lastName = 'Smith';
+Person.prototype.hasDog = true;
+
+let john = new Person('John', 1990, 'teacher');
+let jane = new Person('Jane', 1969, 'designer');
+let mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+console.log(john.hasDog);
+console.log(jane.hasDog);
+console.log(mark.hasDog);
+
+
+
+
+
+
+
+
+
+
+
+
